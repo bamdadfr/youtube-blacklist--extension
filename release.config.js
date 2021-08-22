18ff86383fe607134c0a6ea71f09836a17799b45
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-08-22 19:41:23 +0200

    chore(CI): add firefox release

--- release.config.js
+++ release.config.js
@@ -9,12 +9,12 @@ module.exports = {
         ['@semantic-release/exec', {
             'prepareCmd': 'yarn build:prepare ${nextRelease.version} && yarn build',
         }],
-        // ['semantic-release-firefox-add-on', {
-        //     'extensionId': '{}',
-        //     'targetXpi': 'youtube-blacklist--extension-${nextRelease.version}.xpi',
-        //     'artifactsDir': 'packages',
-        //     'channel': 'listed',
-        // }],
+        ['semantic-release-firefox-add-on', {
+            'extensionId': '{7d4f976c-89d5-4711-9863-2edb7a97a09d}',
+            'targetXpi': 'youtube-blacklist--extension-${nextRelease.version}.xpi',
+            'artifactsDir': 'packages',
+            'channel': 'listed',
+        }],
         // ['semantic-release-chrome', {
         //     'extensionId': '',
         //     'asset': 'youtube-blacklist--extension-${nextRelease.version}.zip',
