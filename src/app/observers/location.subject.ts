53a9e8fe1f594a36af0e87e981eb55d7395f7879
Bamdad Sabbagh <hi@bamdad.fr>
2022-02-07 20:27:34 +0100

    perf(Observers): Make a parent abstract `AbstractSubject` to extend concrete subjects from

--- src/app/observers/location.subject.ts
+++ src/app/observers/location.subject.ts
@@ -1,14 +1,12 @@
 import {AbstractSubject} from './abstract.subject';
-import {AbstractObserver} from './abstract.observer';
 
-export class LocationSubject implements AbstractSubject {
+export class LocationSubject extends AbstractSubject {
   private savedHref: string;
 
   private observer: MutationObserver;
 
-  private observers: AbstractObserver[] = [];
-
   public constructor() {
+    super();
     this.savedHref = window.location.href;
 
     this.observer = new MutationObserver(() => {
@@ -22,29 +20,6 @@ export class LocationSubject implements AbstractSubject {
 
     this.observer.observe(document.body, {
       childList: true,
-    });
-  }
-
-  public attach(observer: AbstractObserver): void {
-    if (this.observers.includes(observer)) {
-      return;
-    }
-
-    this.observers.push(observer);
-  }
-
-  public detach(observer: AbstractObserver): void {
-    const observerIndex = this.observers.indexOf(observer);
-    if (observerIndex === -1) {
-      return;
-    }
-
-    this.observers.splice(observerIndex, 1);
-  }
-
-  public notify(): void {
-    this.observers.forEach((observer) => {
-      observer.update(this);
     });
   }
 }
